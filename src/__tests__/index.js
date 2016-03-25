e245a7b5e2ffc782b4f06ab8323730a703ff48d5
Duncan Beevers <duncan@dweebd.com>
2016-03-25 14:08:48 -0600

    More wibbling

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -2,7 +2,6 @@ import ava from 'ava';
 import postcss from 'postcss';
 import plugin from '..';
 import {name} from '../../package.json';
-import testRemovals from './testRemovals';
 
 const tests = [{
     message: 'should remove empty @ rules',
@@ -50,6 +49,28 @@ const tests = [{
     expected: '',
     removedSelectors: ['h1', '.hot', '.a.b', '', 'h1,h2']
 }];
+
+function testRemovals (t, test, out) {
+    const message = out.messages.find((m) =>
+        m.plugin === 'postcss-discard-empty' && m.type === 'selectors'
+    );
+
+    const selectors = message.selectors;
+
+    test.removedSelectors.forEach((selector) => {
+        const i = selectors.indexOf(selector);
+
+        if (i === -1) {
+            t.fail('expected selector `' + selector + '` was not removed');
+        } else {
+            selectors.splice(i, 1);
+        }
+    });
+
+    selectors.forEach((selector) => {
+        t.fail('unexpected selector `' + selector + '` was removed');
+    });
+}
 
 tests.forEach(test => {
     ava(test.message, t => {
